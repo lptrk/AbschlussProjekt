bdbe5dbf5ab790bcec7cdce3f3ce2e8d2959db67
Patrik <lktpriv@gmail.com>
2023-02-06 09:12:25 +0100

    generated welcome, sidebar and table components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    SidebarComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
